5ac277f2364a870d539b9e92439cddfcf670c701
Mikolaj Kaminski <mikolaj.kaminski737@gmail.com>
2022-03-26 14:48:13 +0100

    homework hints added

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 console.log("Potężna wichura łamiąc duże drzewa, trzciną zaledwie tylko kołysze.");
-let Tytus = document.querySelector(".js-Tytus")
-let Sebek = document.querySelector(".js-Sebek")
-let Janusz = document.querySelector(".js-Janusz")
+let tytus = document.querySelector(".js-tytus")
+let sebek = document.querySelector(".js-sebek")
+let janusz = document.querySelector(".js-janusz")
 let buttonZaloga = document.querySelector(".js-buttonZaloga")
 buttonZaloga.addEventListener("click", () => {
     switch (buttonZaloga.innerText) {
         case "Pokaż Sebka":
             buttonZaloga.innerText = "Pokaż Janusza"
-            Sebek.classList.add("Show")
-            Sebek.classList.remove("bomba__Sebek")
-            Tytus.classList.add("bomba__Tytus")
-            Tytus.classList.remove("Show")
+            sebek.classList.add("bomba__image--show")
+            sebek.classList.remove("bomba__sebek")
+            tytus.classList.add("bomba__tytus")
+            tytus.classList.remove("bomba__image--show")
             break;
         case "Pokaż Janusza":
             buttonZaloga.innerText = "Pokaż Tytusa"
-            Janusz.classList.add("Show")
-            Janusz.classList.remove("bomba__Janusz")
-            Sebek.classList.add("bomba__Sebek")
-            Sebek.classList.remove("Show")
+            janusz.classList.add("bomba__image--show")
+            janusz.classList.remove("bomba__janusz")
+            sebek.classList.add("bomba__sebek")
+            sebek.classList.remove("bomba__image--show")
 
             break;
         case "Pokaż Tytusa":
             buttonZaloga.innerText = "Pokaż Sebka"
-            Janusz.classList.add("bomba__Janusz")
-            Janusz.classList.remove("Show")
-            Tytus.classList.add("Show")
-            Tytus.classList.remove("bomba__Tytus")
+            janusz.classList.add("bomba__janusz")
+            janusz.classList.remove("bomba__image--show")
+            tytus.classList.add("bomba__image--show")
+            tytus.classList.remove("bomba__tytus")
             break;
     }
 })
